d4d08e795e96b34e33995270a87166b3fde0355f
sykwer <sykwer@gmail.com>
2026-03-17 21:18:43 +0900

    Rebase from 'release/humble/agnocast_e2e_test'

--- src/test_ros2_publisher.cpp
+++ src/test_ros2_publisher.cpp
@@ -1,14 +1,15 @@
-#include "agnocast/agnocast.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 #include "std_msgs/msg/int64.hpp"
 
 #include <chrono>
+#include <memory>
 #include <string>
+#include <thread>
 
 using namespace std::chrono_literals;
 
-class TestPublisher : public rclcpp::Node
+class TestROS2Publisher : public rclcpp::Node
 {
   enum class State {
     WaitingForConnection,      // Waiting for subscriber connections
@@ -17,10 +18,11 @@ class TestPublisher : public rclcpp::Node
   };
 
   rclcpp::TimerBase::SharedPtr timer_;
-  agnocast::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
+  rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
   int64_t count_;
   int64_t target_pub_num_;
-  int64_t planned_sub_count_;
+
+  size_t planned_sub_count_;
   size_t planned_pub_count_;
   std::string topic_name_;
 
@@ -31,14 +33,11 @@ class TestPublisher : public rclcpp::Node
 
   bool check_connection_counts()
   {
-    const auto total_sub_count =
-      publisher_->get_subscription_count() + publisher_->get_intra_subscription_count();
     if (
-      total_sub_count < planned_sub_count_ ||
+      publisher_->get_subscription_count() < planned_sub_count_ ||
       this->count_publishers(topic_name_) < planned_pub_count_) {
       return false;
     }
-
     return true;
   }
 
@@ -68,9 +67,10 @@ class TestPublisher : public rclcpp::Node
 
   void publish_message()
   {
-    agnocast::ipc_shared_ptr<std_msgs::msg::Int64> message = publisher_->borrow_loaned_message();
+    auto message = std::make_unique<std_msgs::msg::Int64>();
     message->data = count_;
     publisher_->publish(std::move(message));
+
     RCLCPP_INFO(this->get_logger(), "Publishing %ld.", count_);
     count_++;
 
@@ -86,7 +86,8 @@ class TestPublisher : public rclcpp::Node
   }
 
 public:
-  explicit TestPublisher(const rclcpp::NodeOptions & options) : Node("test_publisher", options)
+  explicit TestROS2Publisher(const rclcpp::NodeOptions & options)
+  : Node("test_ros2_publisher", options)
   {
     this->declare_parameter<std::string>("topic_name", "/test_topic");
     this->declare_parameter<int64_t>("qos_depth", 10);
@@ -96,35 +97,40 @@ public:
     this->declare_parameter<int64_t>("planned_sub_count", 1);
     this->declare_parameter<int64_t>("planned_pub_count", 1);
     this->declare_parameter<bool>("forever", false);
+
     topic_name_ = this->get_parameter("topic_name").as_string();
     int64_t qos_depth = this->get_parameter("qos_depth").as_int();
     bool transient_local = this->get_parameter("transient_local").as_bool();
     int64_t init_pub_num = this->get_parameter("init_pub_num").as_int();
     int64_t pub_num = this->get_parameter("pub_num").as_int();
+
     planned_sub_count_ = this->get_parameter("planned_sub_count").as_int();
-    planned_pub_count_ = static_cast<size_t>(this->get_parameter("planned_pub_count").as_int());
+    planned_pub_count_ = this->get_parameter("planned_pub_count").as_int();
     forever_ = this->get_parameter("forever").as_bool();
 
     rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(qos_depth));
     if (transient_local) {
       qos.transient_local();
+      qos.reliable();
     }
-    publisher_ = agnocast::create_publisher<std_msgs::msg::Int64>(this, topic_name_, qos);
+
+    publisher_ = this->create_publisher<std_msgs::msg::Int64>(topic_name_, qos);
+
     count_ = 0;
     target_pub_num_ = init_pub_num + pub_num;
 
     // Initial publish
     for (int i = 0; i < init_pub_num; i++) {
-      agnocast::ipc_shared_ptr<std_msgs::msg::Int64> message = publisher_->borrow_loaned_message();
+      auto message = std::make_unique<std_msgs::msg::Int64>();
       message->data = count_;
       publisher_->publish(std::move(message));
       RCLCPP_INFO(this->get_logger(), "Publishing %ld.", count_);
       count_++;
     }
 
-    timer_ = this->create_wall_timer(100ms, std::bind(&TestPublisher::timer_callback, this));
+    timer_ = this->create_wall_timer(100ms, std::bind(&TestROS2Publisher::timer_callback, this));
   }
 };
 
 #include <rclcpp_components/register_node_macro.hpp>
-RCLCPP_COMPONENTS_REGISTER_NODE(TestPublisher)
+RCLCPP_COMPONENTS_REGISTER_NODE(TestROS2Publisher)
