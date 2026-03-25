4d399e9cd4cc18901fad102c9160066298fb4431
sykwer <sykwer@gmail.com>
2026-03-25 10:55:50 +0900

    Revert "Trimmed the branch to only the src/agnocast_cie_config_msgs sub directory"
    This reverts commit 4e8ca42b3d0d459e4a187bc8c1720854b41e8d21.

---
+++ agnocast_kmod/agnocast_kunit/agnocast_kunit_get_node_publisher_topics.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <kunit/test.h>
+
+#define TEST_CASES_GET_NODE_PUBLISHER_TOPICS                   \
+  KUNIT_CASE(test_case_get_node_pub_topics_exact_match),       \
+    KUNIT_CASE(test_case_get_node_pub_topics_prefix_no_match), \
+    KUNIT_CASE(test_case_get_node_pub_topics_buffer_size_exceeded)
+
+void test_case_get_node_pub_topics_exact_match(struct kunit * test);
+void test_case_get_node_pub_topics_prefix_no_match(struct kunit * test);
+void test_case_get_node_pub_topics_buffer_size_exceeded(struct kunit * test);
