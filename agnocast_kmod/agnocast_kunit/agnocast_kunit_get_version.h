01ae79c1487d39bd77a4a6982ebd9536c4154706
sykwer <sykwer@gmail.com>
2026-05-21 13:55:15 +0900

    Trimmed the branch to only the src/agnocast sub directory

--- agnocast_kmod/agnocast_kunit/agnocast_kunit_get_version.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
-#pragma once
-#include <kunit/test.h>
-
-#define TEST_CASES_GET_VERSION KUNIT_CASE(test_case_get_version)
-
-void test_case_get_version(struct kunit * test);
